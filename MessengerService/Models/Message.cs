9a3b47a8f420561aaab27f3b158802f6aee658d2
Thea Stewart <cournalpwn@gmail.com>
2025-01-25 22:31:14 +0100

    Add project files.

---
+++ MessengerService/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace MessengerService.Models
+{
+    public class Message
+    {
+        public Guid ID { get; set; }
+        public string RecipientAddress { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime? ReadTimestamp { get; set; }
+        public DateTime SentTimestamp { get; set; }
+    }
+}
---
+++ MessengerService/Models/MessengerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MessengerService.Models
+{
+    public class MessengerDbContext : DbContext
+    {
+        public MessengerDbContext(DbContextOptions<MessengerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; } = null!;
+    }
+}
